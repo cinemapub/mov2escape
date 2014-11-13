0eada7e12c965ce06c8a814fa6d5e2c7d552c6d5
Brightfish Cinemapub <support@cinemapub.be>
2014-11-13 17:58:19 +0100

    faster when source file in right format

--- mov2escape.php
+++ mov2escape.php
@@ -9,7 +9,7 @@ $moddate=date("Y-m-d",filemtime($argv[0]));
 $ffmpeg='C:\tools\ffmpeg64\ffmpeg.exe';
 $magick='C:\Program Files\graphicsmagick-1.3.20-q16\gm.exe';
 $identify='C:\tools\identify.exe';
-$testsec=5;
+$testsec=10;
 
 $dtemp="temp";
 $dout="output";
@@ -81,6 +81,26 @@ if(!file_exists($input)){
 	exit(1);
 }
 
+$ff_src=ff_details($input);
+
+/*
+    [DURATION] => 00:01:06.92, start: 0.000000, bitrate: 97228 kb/s
+    [DUR_FRAMES] => 1606
+    [DUR_LENGTH] => 00:01:06.92
+    [DUR_SECS] => 66.92
+    [FOUND_AUDIO] => 0
+    [FOUND_VIDEO] => 1
+    [PROGRAM] => C:\tools\ffmpeg64\ffmpeg.exe
+    [SIZE_DIMENSIONS] => 6416x1080
+    [SIZE_HEIGHT] => 1080
+    [SIZE_WIDTH] => 6416
+    [VIDEO] => h264 (high 4:4:4 predictive) (avc1 / 0x31637661), yuv444p, 6416x1080, 97227 kb/s, 24 fps, 24 tbr, 12288 tbn, 48 tbc (default)
+    [VID_CODEC] =>  h264 (High 4:4:4 Predictive) (avc1 / 0x31637661)
+    [VID_FPS] => 24
+    [VID_KBPS] => 97227
+*/
+$orig_w=$ff_src["SIZE_WIDTH"];
+$orig_h=$ff_src["SIZE_HEIGHT"];
 trace("ESCAPE HEIGTH: $escp_h");
 trace("LEFT   SCREEN : $cutl_w * $escp_h");
 trace("CENTER SCREEN : $cutc_w * $escp_h");
@@ -91,43 +111,47 @@ trace("ESCAPE WIDTH  : $escp_w");
 /// ------------------ RESCALE TO ESCAPE SIZE
 /// ----------------------------------
 // based on https://trac.ffmpeg.org/wiki/Encode/H.264
-$ffparam[]="-vf \"scale=$escp_w:-1,crop=$escp_w:$escp_h\""; 	// rescale to full escape width / crop center
-$ffparam[]="-c:v libx264 -preset ultrafast -crf 18"; 			// lossless compression
-$ffparam[]="-c:a copy";
-$ffparams=implode(" ",$ffparam);
-
-
-$ftemp="$dtemp\\$prefix.sscope.mp4";
-$flog="log/" . basename($ftemp) . ".log";
-if(do_if_necessary($input,$ftemp)){
-	trace("SUPERSCOPE:    $ftemp","INFO");
-	trace("FFMPEG: $ffparams");
-	cmdline("\"$ffmpeg\" -i \"$input\" $ffparams -y \"$ftemp\" 2> \"$flog\"");
+if($escp_h == $orig_h AND $escp_w == $orig_w){
+	trace("SUPERSCOPE: already in right format","INFO");
+	$ffparam[]="-c:v copy"; 			// almost lossless compression
+	$ffparam[]="-c:a copy";
+} else {
+	trace("SUPERSCOPE: width $orig_w => $escp_w - crop to height $escp_h","INFO");
+	$ffparam[]="-vf \"scale=$escp_w:-1,crop=$escp_w:$escp_h\""; 	// rescale to full escape width / crop center
+	$ffparam[]="-c:v libx264 -preset ultrafast -crf 1"; 			// almost lossless compression
+	$ffparam[]="-c:a copy";
 }
 
-$out_l="$dtemp/$prefix.out_l.mp4";
-$out_r="$dtemp/$prefix.out_r.mp4";
-$out_c="$dtemp/$prefix.out_c.mp4";
+$ftemp="$dtemp\\$prefix.sscope.mp4";
+if(do_if_necessary($input,$ftemp)){
+	run_ffmpeg($input,$ftemp,$ffparam);
+}
+
+$out_l="$dtemp/$prefix.hd_l.mp4";
+$out_r="$dtemp/$prefix.hd_r.mp4";
+$out_c="$dtemp/$prefix.scope_c.mp4";
 
 /// ----------------------------------
 /// ------------------ CUT IN 3 MOVIES
 /// ----------------------------------
 
-$ffcut="-acodec copy -c:v libx264 -preset ultrafast -crf 18";
+$ffcut="-an -c:v libx264 -preset ultrafast -crf 1";
 
 if(do_if_necessary($ftemp,$out_l)){
 	trace("CUT LEFT SCREEN $out_l","INFO");
-	cmdline("\"$ffmpeg\" -i \"$ftemp\" $ffcut -vf \"crop=$cutl_w:$escp_h:0:0,scale=1920:1080\" -y \"$out_l\" 2> \"$flog\"");
+	//run_ffmpeg($ftemp,$out_l,"$ffcut -vf \"crop=$cutl_w:$escp_h:0:0,scale=1920:1080\"");
+	run_ffmpeg($ftemp,$out_l,"$ffcut -vf \"crop=$cutl_w:$escp_h:0:0\"");
 }
 
 if(do_if_necessary($ftemp,$out_r)){
 	trace("CUT RIGHT SCREEN $out_r","INFO");
-	cmdline("\"$ffmpeg\" -i \"$ftemp\" $ffcut -vf \"crop=$cutl_w:$escp_h:in_w-$cutr_w:0,scale=1920:1080\" -y \"$out_r\" 2> \"$flog\"");
+//	run_ffmpeg($ftemp,$out_r,"$ffcut -vf \"crop=$cutl_w:$escp_h:in_w-$cutr_w:0,scale=1920:1080\"");
+	run_ffmpeg($ftemp,$out_r,"$ffcut -vf \"crop=$cutl_w:$escp_h:in_w-$cutr_w:0\"");
 }
 
 if(do_if_necessary($ftemp,$out_c)){
 	trace("CUT CENTER SCREEN $out_c","INFO");
-	cmdline("\"$ffmpeg\" -i \"$ftemp\" $ffcut -vf \"crop=$cutl_w:$escp_h:$cutl_w:0,scale=2048:858\" -y \"$out_c\" 2> \"$flog\"");
+	run_ffmpeg($ftemp,$out_c,"$ffcut -vf \"crop=$cutl_w:$escp_h:$cutl_w:0,scale=2048:858\"");
 }
 
 /// ----------------------------------
@@ -140,12 +164,12 @@ $d_R1="$dout\\$prefix.R";
 $d_R2="$dout\\$prefix.R.dpx";
 $d_C="$dout\\$prefix.C";
 
-render_frames($out_l,$d_L1,"jpg");
+render_frames($out_l,$d_L2,"dpx");
 render_frames($out_c,$d_C,"jpg");
-render_frames($out_r,$d_R1,"jpg");
+render_frames($out_r,$d_R2,"dpx");
 
-convert_dpx($d_L1,$d_L2);
-convert_dpx($d_R1,$d_R2);
+//convert_dpx($d_L1,$d_L2);
+//convert_dpx($d_R1,$d_R2);
 
 
 function render_frames($mov,$folder,$type="dpx"){
@@ -170,7 +194,10 @@ function render_frames($mov,$folder,$type="dpx"){
 		$ffimg="$bname.%06d.jpg";
 		break;;
 	case "dpx":
-		$ffparam="-pix_fmt yuv444p10be -q:v 1";
+		// Input #0, dpx_pipe, from 'DD_TRAILER_SCREEN_23_98_LEFT_grd001.00200.dpx':
+		//   Stream #0:0: Video: dpx, gbrp10le, 1920x1080 [SAR 1:1 DAR 16:9], 25 tbr, 25 tbn, 25 tbc
+		// just the gamma still has to be added
+		$ffparam="-pix_fmt gbrp10le -q:v 1";
 		$ffimg="$bname.%06d.dpx";
 		break;;
 	default:
@@ -181,7 +208,34 @@ function render_frames($mov,$folder,$type="dpx"){
 	$first="$folder\\".sprintf($ffimg,1);
 	if(do_if_necessary($mov,$first)){
 		trace("RENDER FRAMES FOR $mov [$type]","INFO");
-		cmdline("\"$ffmpeg\" -i \"$mov\" $ffparam -y \"$folder\\$ffimg\" 2>> \"$flog\"");	
+		$t1=microtime(true);
+		cmdline("\"$ffmpeg\" -i \"$mov\" $ffparam -y \"$folder\\$ffimg\" 2>> \"$flog\"");
+		$t2=microtime(true);
+	}
+}
+
+function run_ffmpeg($input,$output,$ffparam){
+	global $ffmpeg;
+	
+	$binput=basename($input);
+	$sinput=filesize($input);
+	$minput=round($sinput/1000000,1)."MB";
+	$uniq=substr(md5("$input$ffparam$output"),0,8);
+	if(is_array($ffparam)){
+		$ffparam=implode(" ",$ffparam);
+	}
+	$flog="$binput.$uniq.log";
+	trace("RUN ffmpeg on $binput - $minput","INFO");
+	trace("    $ffparam");
+	trace("    log to $flog");
+	$t1=microtime(true);
+	exec("\"$ffmpeg\" -i \"$input\" $ffparam -y \"$output\" 2>> \"log\\$flog\"");
+	$t2=microtime(true);
+	if(contains($output,'%')){
+		// render to frame files
+	} else {
+		// render to single file
+		
 	}
 }
 
@@ -228,6 +282,127 @@ function convert_dpx($folderin,$folderout){
 		}
 	}
 
+}
+
+function ff_details($file){
+	global $ffmpeg;
+	
+	$program=$ffmpeg;
+	$output=cmdline("\"$ffmpeg\" -i \"$file\" 2>&1");
+	//print_r($output);
+	$result=Array();
+	$result["FOUND_VIDEO"]=0;
+	$result["FOUND_AUDIO"]=0;
+	foreach($output as $line){
+		if(contains($line,"Stream")){
+			if(contains($line,"Video:")){
+				$result["FOUND_VIDEO"]=1;
+				$data=quickmatch($line,"|Video:(.*)|");
+				$result["VIDEO"]=trim(txt_removespecialchars($data));
+				$result["SIZE_DIMENSIONS"]=quickmatch($data,"|(\d\d+x\d\d+)|");
+				$result["VID_FPS"]=quickmatch($data,"|([\d\.]+) fps|");
+				$result["VID_KBPS"]=quickmatch($data,"|(\d+) kb/s|");
+				list($codec,$rest)=explode(",",$data);
+				$result["VID_CODEC"]=$codec;
+			}
+			if(contains($line,"Audio:")){
+				$result["FOUND_AUDIO"]=1;
+				$data=quickmatch($line,"|Audio:(.*)|");
+				$result["AUDIO"]=trim(txt_removespecialchars($data));
+				$result["CHANNELS"]=quickmatch($data,"|(\d) channels|");
+				$result["AUD_KBPS"]=quickmatch($data,"|(\d+) kb/s|");
+				$result["HZ"]=quickmatch($data,"|(\d\d\d\d\d)|");
+			}
+
+		}
+		if(contains($line,"Duration:")){
+			$data=quickmatch($line,"|Duration:(.*)|");
+			$result["DURATION"]=trim(txt_removespecialchars($data));
+			$result["DUR_LENGTH"]=quickmatch($data,"|(\d\d:\d\d:\d\d\.\d\d)|");
+		}	
+	}
+	$result["PROGRAM"]=$program;
+	if($result["DUR_LENGTH"] AND $result["VID_FPS"]){
+		$lenparts=explode(":",$result["DUR_LENGTH"]);
+		$secs=((int)$lenparts[0]*3600) + ((int)$lenparts[1]*60) + (double)$lenparts[2];
+		$frames=round($secs*(double)$result["VID_ FPS"]);
+		$result["DUR_SECS"]=number_format($secs,2);
+		$result["DUR_FRAMES"]=$frames;
+	}
+	if($result["SIZE_DIMENSIONS"]){
+		list($result["SIZE_WIDTH"],$result["SIZE_HEIGHT"])=explode("x",$result["SIZE_DIMENSIONS"],2);
+	}
+	ksort($result);
+	return $result;
+}
+
+function quickmatch($text,$search){
+
+	$nb=preg_match($search , $text, $matches);
+	if($nb){
+		$value=$matches[1];
+		return $value;
+	}
+	return false;
+}
+
+
+function txt_removespecialchars($input){
+	$return=utf8_decode($input);
+	$return=strtolower($input);
+	$return=str_replace(	Array("â","à","ä","ã"),	"a",$return);
+	$return=str_replace(	Array("ç"),				"c",$return);
+	$return=str_replace(	Array("é","è","ë","ê"),	"e",$return);
+	$return=str_replace(	Array("î","ï","í"),		"i",$return);
+	$return=str_replace(	Array("ñ"),				"n",$return);
+	$return=str_replace(	Array("ô","ö","ò","ó"),	"o",$return);
+	$return=str_replace(	Array("œ"),				"oe",$return);
+	$return=str_replace(	Array("ü"),				"u",$return);
+
+	//trace("txt_removespecialchars: output = " . txt_shortentext($return,100));
+	return $return;
+}
+
+function txt_makecanonical($input,$aremove=false,$maxlen=255){
+
+	if(!$aremove){
+		$aremove=Array("the","to","for","and","in");
+	}
+	$areplace=Array();
+	foreach($aremove as $keyword){
+		$areplace[]=txt_removespecialchars(" $keyword ");
+	}
+
+	$return=txt_removespecialchars($input);
+	$return=str_replace($areplace," "," " . $return . " ");
+	$return=str_replace(Array("-","_","'","/",".",",",";",":","!","?","(",")","°","~"),' ',$return);
+	$return2=str_replace(' ','',$return);
+	if(strlen($return2)>$maxlen){
+		$wordend=strpos($return,' ',$maxlen);
+		$return3=substr($return,0,$wordend);
+		if(!$return3){
+			$return3=substr($return,0,$maxlen);
+		}
+
+		$return2=str_replace(' ','',$return3);
+	}
+	trace("txt_makecanonical: output = [$return2] ( " . strlen($return2) . " chars)");
+	return($return2);
+}
+
+function txt_shortentext($text,$max,$etc="…"){
+	if(strlen($text)<=$max) return $text;
+	$text=substr($text,0,$max-strlen($etc)).$etc;
+	trace("txt_shortentext: result= [$text]");
+	return $text;
+}
+
+function txt_removehtml($html,$br="\n"){
+	$result=str_replace("/>","/> ",$html);
+	$result=str_replace(Array("</div>","</p>","<br>","<br />","<br/>"),$br,$html);
+	$result=preg_replace("#<[^>]*>#","",$result);
+	trace("txt_removehtml: output = " . txt_shortentext($result,100));
+	return $result;
 }
 
 ?>
